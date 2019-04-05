d6c5740b74c7a0ff9b521a50dc2b23d82177209f
arizky012 <45511732+arizky012@users.noreply.github.com>
2019-04-05 22:29:01 +0800

    Create KontrolTv.java
    Object

---
+++ 5/no1/KontrolTv.java
@@ -0,0 +1,7 @@
+interface KontrolTv{
+    public void tvOn(boolean on);
+    public void moveChannel(int channel);
+    public void turnUpSound(int plus);
+    public void turnDownSound(int less);
+    public void showMerk();
+}
