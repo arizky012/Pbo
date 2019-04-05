0c6a0d46722a8b4c226a5018000cee76c650058f
arizky012 <45511732+arizky012@users.noreply.github.com>
2019-04-05 22:33:46 +0800

    Create Pegawai.java

---
+++ 6/no1/Pegawai.java
@@ -0,0 +1,14 @@
+class Pegawai{
+    private String nama;
+    private int gaji = 2500000;
+  
+    Pegawai(String n){
+        nama = n;
+    }public String getNama() {
+        return nama;
+    }public int getGaji() {
+        return gaji;
+    }public void setNama(String nama) {
+        this.nama = nama;
+    }   
+}
