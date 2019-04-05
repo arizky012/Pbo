22a320ddce2df446a8cfc813d5b5db5b20a926ec
arizky012 <45511732+arizky012@users.noreply.github.com>
2019-04-05 22:28:16 +0800

    Create Main.java
    Main Method

---
+++ 5/no1/Main.java
@@ -0,0 +1,36 @@
+class Main{
+    static Scanner arzk = new Scanner(System.in);
+    static int input;
+    static KontrolTv tv = new Tv("Samsung");
+    public static void main(String[]args){
+        Boolean power;
+        
+        System.out.println("Menu \n1. Tv ON\n2. Tv OFF\n");
+        input = arzk.nextInt();
+        switch(input){
+            case 1 : power(true);subMenu();break;
+            case 2 : power(false);break;
+            default : System.out.println("not valid");
+        }
+    }
+    static void power(boolean p){
+        tv.tvOn(p);
+        tv.showMerk();
+    }
+    static void subMenu(){
+        System.out.println("MENU");
+            System.out.println("1. Move Channeel\n2. Volume Up\n3. Volume Down");
+            switch(arzk.nextInt()){
+                case 1 : 
+                System.out.print("input : ");
+                tv.moveChannel(arzk.nextInt());break;
+                case 2 : 
+                System.out.print("input : ");
+                tv.turnUpSound(arzk.nextInt());break;
+                case 3 : 
+                System.out.print("input : ");
+                tv.turnDownSound(arzk.nextInt());break;
+                default : System.out.println("not valid");break;
+            }
+    }
+}
